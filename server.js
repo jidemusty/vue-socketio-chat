cbaedea9c298ea5a89db881c9ad9b8b1f24eee28
jidemusty <jidemustapha2013@gmail.com>
2017-10-25 11:44:40 +0100

    socket.io setup

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', () => {
+    console.log('Someone joined');
+})
 
 http.listen(3000, () => {
     console.log('Server Running');
